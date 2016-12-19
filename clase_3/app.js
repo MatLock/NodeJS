fe6d2e55822dcd16432f271c08d307fe9ffb5a74
MatLock <jfflores90@gmail.com>
2016-12-19 20:45:11 -0300

    primer abm en node

--- clase_3/app.js
+++ clase_3/app.js
@@ -1,11 +1,18 @@
 const http = require("http");
 const express = require("express");
+const fs = require("fs");
 const app = express();
 
 const server = http.createServer(app);
 
 let counter = 1;
+let personas = [];
+
 app.use(express.static('./public'));
+
+
+
+
 app.use( (req,res,next) => {
 	console.log("mws 1");
 	req.counter = counter++;
@@ -13,5 +20,31 @@ app.use( (req,res,next) => {
 });
 
 app.get("/counter", (req,res) => res.end( ' ' + req.counter));
+
+
+app.get("/" , (req,res) => {
+	let content = fs.readFileSync("./public/content.html").toString();
+	let append = '';
+	personas.forEach ( alu => {
+		append += '<tr>'+
+						'<td>'+ alu.dni + '</td>' +
+						'<td>'+ alu.nombre + '</td>' +
+						'<td>'+ alu.edad + '</td>'+
+				 '</tr>';
+	});
+	res.end(content.replace('{{data}}',append));
+})
+
+
+app.get("/add", (req,res) => {
+	res.end(fs.readFileSync("./public/add.html"));
+});
+
+app.get("/save" , (req,res) => {
+	personas.push(req.query);
+	console.log(personas);
+	res.redirect('/');
+});
+
 
 server.listen(5000);
