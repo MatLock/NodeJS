1a6b43312c4d360d4bf81570c452509d0a88b8cb
MatLock <jfflores90@gmail.com>
2016-12-19 21:54:34 -0300

    handlebars, express

--- clase_3/app.js
+++ clase_3/app.js
@@ -2,14 +2,19 @@ const http = require("http");
 const express = require("express");
 const fs = require("fs");
 const app = express();
-
+const bodyParser = require('body-parser')
+const exphbs  = require('express-handlebars');
 const server = http.createServer(app);
 
+
 let counter = 1;
-let personas = [];
+let alumnos = [];
 
+app.engine('.hbs', exphbs({extname: '.hbs', defaultLayout:'default'}));
+app.set('view engine', '.hbs');
 app.use(express.static('./public'));
-
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 
 
@@ -21,9 +26,8 @@ app.use( (req,res,next) => {
 
 app.get("/counter", (req,res) => res.end( ' ' + req.counter));
 
-
-app.get("/" , (req,res) => {
-	let content = fs.readFileSync("./public/content.html").toString();
+/*
+let content = fs.readFileSync("./public/content.html").toString();
 	let append = '';
 	personas.forEach ( alu => {
 		append += '<tr>'+
@@ -33,16 +37,24 @@ app.get("/" , (req,res) => {
 				 '</tr>';
 	});
 	res.end(content.replace('{{data}}',append));
+*/
+app.get("/" , (req,res) => {
+	res.render("home", {alumnos:alumnos});
 })
 
 
-app.get("/add", (req,res) => {
-	res.end(fs.readFileSync("./public/add.html"));
-});
+app.get("/add", (req,res) => res.render("form"));
 
-app.get("/save" , (req,res) => {
-	personas.push(req.query);
-	console.log(personas);
+app.post("/save" , (req,res) => {
+	let alum = req.body;
+	alum.fecha = new Date().toString();
+	alumnos.push(alum);
+	fs.writeFile( './security/registro.txt', JSON.stringify(alum) + '\n',{enconding:'utf8',flag: 'a+'} , (err) => {
+		if (err) {
+			console.log('error');
+		}
+	});
+	//res.json(req.body);
 	res.redirect('/');
 });
 
